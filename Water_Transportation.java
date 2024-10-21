e30df18be21924d6e5218c64a29f5ad9434833ac
haleygg <haleybuggy27@gmail.com>
2024-10-21 16:04:15 -0400

    Update Water_Transportation.java
    added more variables to water trans.

--- Water_Transportation.java
+++ Water_Transportation.java
@@ -1,11 +1,21 @@
 public class Water_Transportation extends Transportation{
-    private int waterDisplacement;
+    private String dock;
+    private String loadingTime;
+    private String marina;
 
-    public void setWaterDisplacement(int waterDisplacement) { this.waterDisplacement = waterDisplacement; }
-    public int getWaterDisplacement() {return this.waterDisplacement; }
-
-    @Override
-    public String toString() {
-        return super.toString() + "\nWater Displacement:" + getWaterDisplacement() + "m^3";
+    Water_Transportation(String dock, String loadingTime, String marina,String rentalOrPurchase,double purchasePrice,
+                         double rentalPrice, double averageSpeed, int numPassengers){
+        super(rentalOrPurchase, purchasePrice, rentalPrice, averageSpeed, numPassengers);
+        this.dock = dock;
+        this.loadingTime = loadingTime;
+        this.marina = marina;
     }
+    public String getDock(){return dock;}
+    public String getLoadingTime() {return loadingTime;}
+    public String getMarina() {return marina;}
+
+    public void setDock(String dock){this.dock = dock;}
+    public void setLoadingTime(String loadingTime) {this.loadingTime=loadingTime;}
+    public void setMarina(String marina) { this.marina=marina;}
+
 }
