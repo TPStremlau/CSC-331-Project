733a49428e11266685e263818ed53e075671adf3
haleygg <haleybuggy27@gmail.com>
2024-10-21 16:05:32 -0400

    Update Air_Transportation.java
    added more variables to air trans.

--- Air_Transportation.java
+++ Air_Transportation.java
@@ -1,11 +1,29 @@
-public class Air_Transportation extends Transportation{
-    private int altitude;
+public class AirTransportation extends Transportation{
+    private String airport;
+    private String departureTime;
+    private String airline;
+    private String arrivalTime;
 
-    public void setAltitude(int altitude) { this.altitude = altitude; }
-    public int getAltitude() {return this.altitude; }
+    AirTransportation(String airport,String airline, String departureTime, String arrivalTime,String rentalOrPurchase,
+                      double purchasePrice, double rentalPrice, double averageSpeed, int numPassengers){
+        super(rentalOrPurchase, purchasePrice, rentalPrice, averageSpeed, numPassengers);
+        this.airport = airport;
+        this.airline = airline;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
 
-    @Override
-    public String toString() {
-        return super.toString() + "\nAltitude:" + getAltitude();
+    public String getAirport() {return this.airport;}
+    public String getAirline() {return this.airline;}
+    public String getDepartureTime() {return this.departureTime;}
+    public String getArrivalTime() {return this.arrivalTime;}
+
+    public void setAirport(String airport) {this.airport =airport;}
+    public void setAirline(String airline) {this.airline = airline;}
+    public void setDepartureTime(String departureTime) {this.departureTime = departureTime;}
+    public void setArrivalTime(String arrivalTime) {this.arrivalTime=arrivalTime;}
+
+    public String get_arrival_info(){
+        return "Arrive at"+getArrivalTime()+" at "+getAirport();
     }
 }
