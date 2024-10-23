75489cf731ff380b286b4028a8f9b6e8ef9a492e
TPStremlau <tpstremlau@gmail.com>
2024-10-23 13:34:59 -0400

    Update Land_Transportation.java
    Added Constructor and changed the format of the toString() method

--- Land_Transportation.java
+++ Land_Transportation.java
@@ -1,12 +1,18 @@
 public class Land_Transportation extends Transportation {
     private String fuelType;
 
+    Land_Transportation(String rentalOrPurchase, double purchasePrice, double rentalPrice, double averageSpeed,
+            int numPassengers, String fuelType){
+        super(rentalOrPurchase, purchasePrice, rentalPrice, averageSpeed, numPassengers);
+        this.fuelType = fuelType;
+    }
+
     public void setFuelType(String fuelType) { this.fuelType = fuelType; }
     public String getFuelType() { return this.fuelType; }
 
     @Override
     public String toString() {
-        return super.toString() + "\nFuel Type:" + getFuelType();
+        return super.toString() + String.format("%25s %10s", "Fuel-Type:", getFuelType());
     }
 }
 
