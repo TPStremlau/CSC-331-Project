0843e0e82aac291154799e4b53faee8c9cc5743d
TPStremlau <tpstremlau@gmail.com>
2024-10-24 17:59:03 -0400

    Update Transportation.java

--- Transportation.java
+++ Transportation.java
@@ -1,14 +1,10 @@
-
-
 public class Transportation {
-    private String rentalOrPurchase;
     private double purchasePrice;
     private double rentalPrice;
     private double averageSpeed;
     private int numPassengers;
 
-    Transportation(String rentalOrPurchase, double purchasePrice, double rentalPrice, double averageSpeed, int numPassengers) {
-        this.rentalOrPurchase = rentalOrPurchase;
+    Transportation(double purchasePrice, double rentalPrice, double averageSpeed, int numPassengers) {
         this.purchasePrice = purchasePrice;
         this.rentalPrice = rentalPrice;
         this.averageSpeed = averageSpeed;
@@ -16,14 +12,12 @@ public class Transportation {
     }
 
     // accessors
-    public String getRentalOrPurchase() {return rentalOrPurchase;}
     public double getPurchasePrice() { return purchasePrice; }
     public double getRentalPrice() { return rentalPrice; }
     public double getAverageSpeed() { return averageSpeed; }
     public int getNumPassengers() { return numPassengers; }
 
     // mutators
-    public void setRentalOrPurchase(String rentalOrPurchase) {this.rentalOrPurchase = rentalOrPurchase;}
     public void setPurchasePrice(double purchasePrice) {this.purchasePrice=purchasePrice;}
     public void setRentalPrice(double rentalPrice) { this.rentalPrice = rentalPrice;}
     public void setAverageSpeed(double averageSpeed) { this.averageSpeed = averageSpeed;}
@@ -31,8 +25,12 @@ public class Transportation {
 
     @Override
     public String toString(){
-        return String.format("%-25s%-10s%-25s%-10f%-25s%-10f%-25s%-10f%-25s%-10d","Rental or Purchase:",getRentalOrPurchase(),
-                "Purchase price:",getPurchasePrice(),"Rental price:",getRentalPrice(),"Average speed:",
+        return String.format("%-25s%-10f%-25s%-10f%-25s%-10f%-25s%-10d",
+                "Ticket purchase price:",getPurchasePrice(),"Rental price:",getRentalPrice(),"Average speed:",
                 getAverageSpeed(),"Num of passengers:",getNumPassengers());
+    }
+
+    public String getName() {
+        return "";
     }
 }
