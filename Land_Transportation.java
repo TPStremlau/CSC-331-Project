fdaddf8bc69a02846a7170ad85297002d89c0255
haleygg <haleybuggy27@gmail.com>
2024-10-24 21:08:47 -0400

    Update Land_Transportation.java
    updated land trans.

--- Land_Transportation.java
+++ Land_Transportation.java
@@ -1,22 +1,27 @@
 public class Land_Transportation extends Transportation {
     private String fuelType;
 
-    public Land_Transportation(double purchasePrice, double rentalPrice, double averageSpeed,
-                               int numPassengers, String fuelType){
+    public Land_Transportation(double purchasePrice, double rentalPrice,
+                               double averageSpeed, int numPassengers, String fuelType){
         super(purchasePrice, rentalPrice, averageSpeed, numPassengers);
         this.fuelType = fuelType;
+
     }
 
     public void setFuelType(String fuelType) { this.fuelType = fuelType; }
+
     public String getFuelType() { return this.fuelType; }
+
 
     @Override
     public String toString() {
-        return super.toString() + String.format("%25s %10s", "Fuel-Type:", getFuelType());
+        return super.toString() + String.format("%-25s %-10s", "Fuel-Type: ", getFuelType());
     }
 
-    @Override
-    public String getName(){
+
+    public String get_arrival_info(){
         return "";
-    };
+    }
+    @Override
+    String getName(){ return "";}
 }
